ea27dcac87d1fc05672313a35547a064d416f57d
sushidesu <hugemerge@gmail.com>
2020-10-10 06:22:34 +0000

    update: add link to home and about

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,15 @@
 import React from "react"
+import Link from "next/link"
 
 export const Layout: React.FC = ({ children }) => (
   <div>
     <header>
-      <p>this is a header</p>
+      <p>
+        <Link href="/">This is a header</Link>
+      </p>
+      <p>
+        <Link href="/about">About</Link>
+      </p>
     </header>
     <main>{children}</main>
   </div>
