0322ccdeb743ccb5ccc0b0db6f1c57f1d4025d26
sushidesu <hugemerge@gmail.com>
2020-10-12 05:41:00 +0000

    Add global styles and theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,27 @@
 import { AppProps } from "next/app"
+import { ThemeProvider } from "emotion-theming"
+import { Global, css } from "@emotion/core"
+import { Theme } from "../plugins/emotion"
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const theme: Theme = {
+  color: {
+    primary: "#ccc",
+    secondary: "#dedede",
+    accent: "#fea",
+  },
+}
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <Global
+      styles={css`
+        body {
+          font-family: sans-serif;
+        }
+      `}
+    />
+    <Component {...pageProps} />
+  </ThemeProvider>
+)
 
 export default App
