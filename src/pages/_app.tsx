1ebe79379d4993a8e699f576b0a2db6ea9e8a8e1
sushidesu <hugemerge@gmail.com>
2020-10-13 10:14:14 +0000

    feat: add geist-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app"
+import { CssBaseline } from "@geist-ui/react"
 import { ThemeProvider } from "emotion-theming"
 import { Global, css } from "@emotion/core"
 import { Theme } from "plugins/emotion"
@@ -13,10 +14,12 @@ const theme: Theme = {
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
+    <CssBaseline />
     <Global
       styles={css`
         body {
-          font-family: sans-serif;
+          font-family: "Inter", "BlinkMacSystemFont",
+            "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;
         }
       `}
     />
