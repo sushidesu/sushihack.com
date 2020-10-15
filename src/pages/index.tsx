21a02b1837bf77584df24f89ac3da276683eb36d
sushidesu <hugemerge@gmail.com>
2020-10-15 04:04:29 +0000

    fix styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { InferGetStaticPropsType, GetStaticProps } from "next"
 import Head from "next/head"
 import NextLink from "next/link"
 import { graphQLClient, gql } from "plugins/graphql"
+import styled from "@emotion/styled"
 import { Grid, Card, Link } from "@geist-ui/react"
 import { Layout } from "components/Layout"
 
@@ -32,25 +33,56 @@ const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Grid.Container gap={2} justify="center">
-        {posts.map((post) => (
-          <Grid xs={24} md={12} key={post.id}>
-            <Card shadow>
-              <NextLink
-                passHref
-                href={"/post/[slug]"}
-                as={`/post/${post.slug}`}
-              >
-                <Link>
-                  <h2>{post.title}</h2>
-                </Link>
-              </NextLink>
-            </Card>
-          </Grid>
-        ))}
-      </Grid.Container>
+      <Section>
+        <Grid.Container gap={2} justify="center">
+          {posts.map((post) => (
+            <Grid xs={24} md={12} key={post.id}>
+              <Card shadow>
+                <Flex>
+                  <Dummy />
+                  <NextLink
+                    passHref
+                    href={"/post/[slug]"}
+                    as={`/post/${post.slug}`}
+                  >
+                    <Link>
+                      <Title>{post.title}</Title>
+                    </Link>
+                  </NextLink>
+                </Flex>
+              </Card>
+            </Grid>
+          ))}
+        </Grid.Container>
+      </Section>
     </Layout>
   )
 }
+
+const Section = styled.section`
+  padding: 3rem 0;
+`
+
+const Dummy = styled.span`
+  display: block;
+  background-color: #bde;
+  width: 80px;
+  height: 80px;
+  border-radius: 0.4rem;
+`
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  & > a {
+    flex: 1 1 auto;
+  }
+`
+
+const Title = styled.h2`
+  margin: 0 0 0 0.8em;
+  font-size: 1.6rem;
+`
 
 export default Home
