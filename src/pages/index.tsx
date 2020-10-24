04da08d04f0f0db012569f388c27ebf073a96d22
sushidesu <hugemerge@gmail.com>
2020-10-24 12:52:47 +0000

    fix style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import React from "react"
 import { InferGetStaticPropsType, GetStaticProps } from "next"
 import Head from "next/head"
 import NextLink from "next/link"
 import { graphQLClient, gql } from "plugins/graphql"
 import styled from "@emotion/styled"
-import { Grid, Card, Link } from "@geist-ui/react"
+import { Grid, Card, Link, Tag, Spacer } from "@geist-ui/react"
 import { Layout } from "components/Layout"
 
 export const getStaticProps: GetStaticProps<{ posts: Post[] }> = async () => {
@@ -55,11 +56,15 @@ const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
                         <Title>{post.title}</Title>
                       </Link>
                     </NextLink>
-                    <div>
+                    <Spacer y={0.5} />
+                    <Tags>
                       {post.tags.map((tag) => (
-                        <span key={tag.id}>{tag.label}</span>
+                        <React.Fragment key={tag.id}>
+                          <Tag type="warning">{tag.label}</Tag>
+                          <Spacer x={0.5} />
+                        </React.Fragment>
                       ))}
-                    </div>
+                    </Tags>
                   </div>
                 </Flex>
               </Card>
@@ -87,6 +92,10 @@ const Dummy = styled.span`
 const Flex = styled.div`
   display: flex;
   justify-content: space-between;
+`
+
+const Tags = styled.div`
+  display: flex;
 `
 
 const Title = styled.h2`
