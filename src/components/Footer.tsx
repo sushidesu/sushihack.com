874db566de075f4a551a82151bcff38d6492db78
sushidesu <hugemerge@gmail.com>
2021-06-06 22:09:14 +0900

    clsxを使う

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
+import clsx from "clsx"
 import { Container } from "./Container"
 
 export const Footer = (): JSX.Element => (
   <footer className="bg-yellow-200">
     <Container>
-      <p className="my-5 text-center">©️ sushidesu</p>
+      <p className={clsx("my-5", "text-center")}>©️ sushidesu</p>
     </Container>
   </footer>
 )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import clsx from "clsx"
 import { Container } from "./Container"
 
 export const Header = (): JSX.Element => (
   <header className="bg-yellow-300">
     <Container>
-      <div className="flex justify-between">
+      <div className={clsx("flex", "justify-between")}>
         <p>
           <Link href="/">This is a header</Link>
         </p>
