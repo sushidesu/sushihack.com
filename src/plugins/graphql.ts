5db6ea0c7dc3c5543cb25dff28ee628c9f4e6556
sushidesu <hugemerge@gmail.com>
2021-06-06 20:48:35 +0900

    fix: エラーメッセージを追加

--- src/plugins/graphql.ts
+++ src/plugins/graphql.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request"
 export { gql } from "graphql-request"
 
 if (!process.env.GRAPHCMS_ENDPOINT) {
-  throw new Error()
+  throw new Error("GRAPHCMS_ENDPOINT is not defined")
 }
 
 export const graphQLClient = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT)
