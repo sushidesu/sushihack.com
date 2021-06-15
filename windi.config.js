a6035434424bffa22a11561c32dce2e80447d4a7
sushidesu <hugemerge@gmail.com>
2021-06-15 17:24:20 +0900

    space-blue色を定義

---
+++ windi.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "windicss/helpers"
+
+export default defineConfig({
+  theme: {
+    extend: {
+      colors: {
+        "space-blue": "#839BC7",
+      },
+    },
+  },
+})
