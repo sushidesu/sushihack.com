4ae188b5706d791ca14c9e43c219e83e2773071e
sushidesu <hugemerge@gmail.com>
2021-09-27 14:51:50 +0900

    use global.css in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,7 @@
 import { AppProps } from "next/app"
-import { ThemeProvider } from "emotion-theming"
-import { Global, css } from "@emotion/core"
-import { Theme } from "plugins/emotion"
 import "windi.css"
+import "../styles/global.css"
 
-const theme: Theme = {
-  color: {
-    primary: "#ccc",
-    secondary: "#dedede",
-    accent: "#fea",
-  },
-}
-
-const App = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider theme={theme}>
-    <Global
-      styles={css`
-        body {
-          font-family: "Inter", "BlinkMacSystemFont",
-            "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;
-        }
-      `}
-    />
-    <Component {...pageProps} />
-  </ThemeProvider>
-)
+const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
 export default App
