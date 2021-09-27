9e67be94846a970e1b00e54f5b55444224537488
sushidesu <hugemerge@gmail.com>
2021-09-27 15:12:43 +0900

    use ArticleCardWrapper.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import { InferGetStaticPropsType, GetStaticProps } from "next"
 import Head from "next/head"
-import clsx from "clsx"
 import { graphQLClient, gql } from "plugins/graphql"
 import { Layout } from "components/Layout/Layout"
 import { Wrapper } from "components/Wrapper/Wrapper"
 import { ArticleCard } from "components/ArticleCard/ArticleCard"
+import { ArticleCardWrapper } from "components/ArticleCard/ArticleCardWrapper"
 
 export const getStaticProps: GetStaticProps<{ posts: Post[] }> = async () => {
   const query = gql`
@@ -41,7 +41,7 @@ const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
 
       <Wrapper>
         <section>
-          <div className={clsx("space-y-6")}>
+          <ArticleCardWrapper>
             {posts.map((post) => (
               <ArticleCard
                 key={post.id}
@@ -54,7 +54,7 @@ const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
                 }))}
               />
             ))}
-          </div>
+          </ArticleCardWrapper>
         </section>
       </Wrapper>
     </Layout>
