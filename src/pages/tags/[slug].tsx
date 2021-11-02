de884d342838ab26f1f0238052fca4d3dd68385b
sushidesu <hugemerge@gmail.com>
2021-11-02 18:00:27 +0900

    fix styles

--- src/pages/tags/[slug].tsx
+++ src/pages/tags/[slug].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next"
 import { Layout } from "components/Layout/Layout"
 import { TagDataWithPosts } from "components/interface/tag-data"
+import { PostItem } from "components/page/tags/PostItem/PostItem"
 import { BlogPostRepository } from "../../infra/blog-post-repository"
 import { getSlug } from "../../utils/getSlug"
 
@@ -9,7 +10,9 @@ const TagPage = ({ tag }: InferGetStaticPropsType<typeof getStaticProps>) => (
     <h1>{tag.label}</h1>
     <ul>
       {tag.posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
+        <li key={post.id}>
+          <PostItem title={post.title} path={`/posts/${post.slug}`} />
+        </li>
       ))}
     </ul>
   </Layout>
