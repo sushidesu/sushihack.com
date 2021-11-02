3c1838f7a1a75127c39b7568f51486d5de420657
sushidesu <hugemerge@gmail.com>
2021-11-02 17:20:07 +0900

    update packages

--- next.config.js
+++ next.config.js
@@ -2,13 +2,12 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 })
 
-module.exports = (_, { defaultConfig }) =>
-  withMDX({
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.node = { fs: "empty", module: "empty" }
-      }
-      return config
-    },
+module.exports = (_, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = withMDX({
     pageExtensions: [...defaultConfig.pageExtensions, "mdx"],
   })
+  return nextConfig
+}
