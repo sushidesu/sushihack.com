f8789dd6f0c21d4b7b0b739c67f7ab33a602fe11
sushidesu <hugemerge@gmail.com>
2023-06-03 11:13:12 +0900

    setup default metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react"
 import "../styles/global.css"
 import { Layout } from "components/ui/Layout/Layout"
+import { Metadata } from "next"
 
 export default function RootLayout({
   children,
@@ -14,4 +15,34 @@ export default function RootLayout({
       </body>
     </html>
   )
+}
+
+export const metadata: Metadata = {
+  icons: {
+    apple: {
+      url: "/apple-touch-icon.png",
+      sizes: "180x180",
+    },
+    icon: [
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon.ico",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
+  title: {
+    default: "sushihack",
+    template: "%s | sushihack",
+  },
+  description: "sushidesu(すし職人)のブログです",
 }
