249aa47a1fbf886aec3eba721e4f45bec41fffcc
sushidesu <hugemerge@gmail.com>
2023-07-09 00:16:54 +0900

    set metadata to about page

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,6 +1,9 @@
 import { ReactNode } from "react"
 import { Wrapper } from "components/ui/Wrapper/Wrapper"
 import { Container } from "components/ui/Container/Container"
+import { Metadata } from "next"
+import { titleTemplate } from "constants/title-template"
+import { format } from "util"
 
 export default function RootLayout({
   children,
@@ -12,4 +15,22 @@ export default function RootLayout({
       <Wrapper>{children}</Wrapper>
     </Container>
   )
+}
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "",
+  twitter: {
+    card: "summary_large_image",
+  },
+  openGraph: {
+    url: "/",
+    title: format(titleTemplate, "About"),
+    images: "/ogp.png",
+    type: "article",
+    description: "",
+  },
+  alternates: {
+    canonical: "/about",
+  },
 }
